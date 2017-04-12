2a7e9124035a095d260bd0722ad15becef48f6b9
Ianhankin <ian.hankin@gmail.com>
2017-04-12 17:22:45 -0400

    Update wrapper.m
    Changed the choice from the full word to just m or l

--- src/wrapper.m
+++ src/wrapper.m
@@ -1,21 +1,21 @@
 %%wrapper
 %Asks the user for input to determine which script to run
 dimensions=0;
-choice='choice';
+choice='c';
 while dimensions<1||dimensions>2
     dimensions=input('Please enter the dimensions of the model (1 or 2):  ');
 end
-while ~strcmp(choice,'latices')&&~strcmp(choice,'magnetization')
-    choice=input('choose the model (latices or magnetization):  ','s');
+while ~strcmp(choice,'m')&&~strcmp(choice,'m')
+    choice=input('choose the model ((l)atices or (m)agnetization):  ','s');
 end
 if dimensions==1
-    if strcmp(choice,'latices')
+    if strcmp(choice,'m')
         advancedlab_ising_1D_p;
     else
         advancedlab_ising_1D_mag;
     end
 else
-    if strcmp(choice,'latices')
+    if strcmp(choice,'l')
         advancedlab_ising_2D;
     else
         advancedlab_ising_2D_energymagmagsusspehea
